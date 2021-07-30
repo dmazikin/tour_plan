6e7ca58797442013f98d92190a8de2e7d42242b0
dmitry_mazikin <mazikindima@mail.ru>
2021-07-30 21:05:07 +0300

    fix: laod lazy map

--- js/main.js
+++ js/main.js
@@ -99,6 +99,11 @@ $(document).ready(function () {
   });
   //Подключение параллакса
   $(".newsletter").parallax({ imageSrc: "img/newsletter-bg.jpg" });
+  //ленивая загрузка
+  $(function () {
+    $(".lazy").Lazy();
+  });
+
   var myMap;
 
   ymaps.ready(init);
@@ -232,18 +237,28 @@ $(document).ready(function () {
             preset: "islands#greenDotIconWithCaption",
           }
         )
-      )
-      .add(
-        new ymaps.Placemark(
-          [7.838196, 98.298813],
-          {
-            balloonContent: "цвет <strong>голубой</strong>",
-          },
-          {
-            preset: "islands#blueCircleDotIconWithCaption",
-            iconCaptionMaxWidth: "50",
-          }
-        )
       );
+  }
+  let map_container = document.getElementById("map");
+  console.log(map_container);
+  let options_map = {
+    once: true, //запуск один раз, и удаление наблюдателя сразу
+    passive: true,
+    capture: true,
+  };
+  map_container.addEventListener("click", start_lazy_map, options_map);
+  map_container.addEventListener("mouseover", start_lazy_map, options_map);
+  map_container.addEventListener("touchstart", start_lazy_map, options_map);
+  map_container.addEventListener("touchmove", start_lazy_map, options_map);
+
+  let map_loaded = false;
+  function start_lazy_map() {
+    if (!map_loaded) {
+      let map_block = document.getElementById("map_lazy");
+      map_loaded = true;
+      map_block.setAttribute("src", map_block.getAttribute("data-src"));
+      map_block.removeAttribute("data_src");
+      console.log(start_lazy_map);
+    }
   }
 });
